f60f5fb3f23dcb21d511bc0a9a825fbdd7a59f08
Spencer Michaels <sxmichaels@gmail.com>
2016-01-04 21:23:23 -0600

    added keyword query

--- cloud.js
+++ cloud.js
@@ -9,6 +9,7 @@ AV.Cloud.define("updateFilters", searchAPIs.updateFilters);
 AV.Cloud.define("getUsersByFilter", searchAPIs.getUsersByFilter);
 //AV.Cloud.define("getUsersByKeyword", searchAPIs.getUsersByKeyword);
 
+AV.Cloud.define("getUsersShortByKeyword", userAPIs.getUsersShortByKeyword);
 AV.Cloud.define("getUserShortById", userAPIs.getUserShortById);
 AV.Cloud.define("signUpWithWeibo", userAPIs.signUpWithWeibo);
 AV.Cloud.define("signUpWithWeixin", userAPIs.signUpWithWeixin);
--- models/user.js
+++ models/user.js
@@ -4,20 +4,11 @@
 
 var AV = require('leanengine');
 
-exports.getUserShortById = function (req, res) {
-    var user = AV.Object.extend('_User');
-    var query = new AV.Query(user);
-    query.equalTo('objectId', req.params.id);
-    query.select('username', 'name', 'category', 'score', 'avatar');
-
-    query.first({
-        success: function (results) {
-            res.success(results);
-        }, error: function (err) {
-            res.error(err);
-        }
-    });
-};
+/** @constant The fields retrieved when querying short user data
+    @type {string[]}
+    @default
+ */
+const USER_SHORT_DATA = ['uid', 'username', 'name', 'category', 'score', 'avatar']
 
 var encapsulateThirdPartyUserdata = function (platform, userData, req, res) {
     var authData = {};
@@ -50,3 +41,53 @@ exports.signUpWithWeixin = function (req, res) {
         "expires_in": 1382686496
     }, req, res)
 };
+
+/**
+ * Given a user ID, gets the essential data representing a user
+ *
+ * @param {string} id - The id of the user.
+ *
+ * @returns The username, name, category, score, and avatar of the user.
+ */
+exports.getUserShortById = function (req, res) {
+    var user = AV.Object.extend('_User');
+    var query = new AV.Query(user);
+
+    query.equalTo('objectId', req.params.id);
+    query.select(USER_SHORT_DATA);
+
+    query.first({
+        success: function (results) {
+            res.success(results);
+        }, error: function (err) {
+            res.error(err);
+        }
+    });
+};
+
+/**
+ * Given a keyword, return a list of users (short data) matching it.
+ *
+ * @param {string} keyword - The keyword to search for.
+ *
+ * @returns The UIDs of all matching users.
+ */
+exports.getUsersShortByKeyword = function (req, res) {
+    var user = AV.Object.extend('User');
+    var query = new AV.Query(user);
+
+    // TODO: Searching by substring match is too slow for small user data sets
+    // This is just a simple placeholder for now
+    query.contains('username', req.keyword.toLowerCase())
+    query.contains('name', req.keyword.toLowerCase())
+
+    query.select(USER_SHORT_DATA)
+
+    query.find({
+        success: function (results) {
+            res.success(results);
+        }, error: function (err) {
+            res.error(err);
+        }
+    });
+}
